129fca9ea634ba67bc8d5c5356661b6a7bd7e276
mio991 <mio991@gmx.de>
2015-03-17 20:42:04 +0900

    Adding DebugLogger

---
+++ Chitter/DebugLogger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ColossalFramework;
+
+using MessageTypeInternal = ColossalFramework.Plugins.PluginManager.MessageType;
+
+namespace Chitter
+{
+    public static class DebugLogger
+    {
+        public static void Log(MessageType type, string message)
+        {
+            MessageTypeInternal typeInternal;
+            switch(type)
+            {
+                default:
+                case MessageType.Error:
+                    typeInternal = MessageTypeInternal.Error;
+                    break;
+                case MessageType.Warning:
+                    typeInternal = MessageTypeInternal.Warning;
+                    break;
+                case MessageType.Message:
+                    typeInternal = MessageTypeInternal.Message;
+                    break;
+            }
+            DebugOutputPanel.AddMessage(typeInternal, message);
+        }
+
+        public static void Message(string message)
+        {
+            Log(MessageType.Message, message);
+        }
+
+        public static void Error(string message)
+        {
+            Log(MessageType.Error, message);
+        }
+
+        public static void Warning(string message)
+        {
+            Log(MessageType.Warning, message);
+        }
+    }
+
+    public enum MessageType
+    {
+        Error,
+        Warning,
+        Message
+    }
+}

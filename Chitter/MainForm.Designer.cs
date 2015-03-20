aab2594639e37333d6d686b92bc580a95fc67db8
mio991 <mio991@gmx.de>
2015-03-20 18:21:08 +0900

    Setting up the MainForm

---
+++ Chitter/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Chitter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.niMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsNotification = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.miClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsNotification.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // niMain
+            // 
+            this.niMain.ContextMenuStrip = this.cmsNotification;
+            this.niMain.Icon = ((System.Drawing.Icon)(resources.GetObject("niMain.Icon")));
+            this.niMain.Text = "Chitter";
+            this.niMain.Visible = true;
+            this.niMain.DoubleClick += new System.EventHandler(this.niDoubleClick);
+            // 
+            // cmsNotification
+            // 
+            this.cmsNotification.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miClose});
+            this.cmsNotification.Name = "cmsNotification";
+            this.cmsNotification.Size = new System.Drawing.Size(104, 26);
+            // 
+            // miClose
+            // 
+            this.miClose.Name = "miClose";
+            this.miClose.Size = new System.Drawing.Size(103, 22);
+            this.miClose.Text = "Close";
+            this.miClose.Click += new System.EventHandler(this.OnClickClose);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 301);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Chitter - Tweet to Cities Skylines";
+            this.SizeChanged += new System.EventHandler(this.OnSizeChanged);
+            this.cmsNotification.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon niMain;
+        private System.Windows.Forms.ContextMenuStrip cmsNotification;
+        private System.Windows.Forms.ToolStripMenuItem miClose;
+    }
+}
+
---
+++ Chitter/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chitter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClickClose(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void niDoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void OnSizeChanged(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = this.WindowState != FormWindowState.Minimized;
+        }
+    }
+}
